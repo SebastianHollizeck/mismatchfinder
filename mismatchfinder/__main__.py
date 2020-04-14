146f755603fcb618aa38b219c8c2776f6b98b69a
SHollizeck <sebastian.hollizeck@gmx.de>
2020-04-14 12:53:54 +1000

    output the result in the end of the processing

--- mismatchfinder/__main__.py
+++ mismatchfinder/__main__.py
@@ -74,6 +74,7 @@ def main():
     #     convertToPandasDataFrame(tumourResults),
     #     normalDf=convertToPandasDataFrame(normalResults),
     # )
+    print(tumourResults[0])
 
 
 if __name__ == "__main__":
