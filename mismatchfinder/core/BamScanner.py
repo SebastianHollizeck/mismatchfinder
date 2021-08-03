e01ae27a62d1cf5da4a6918d6dc5125d28534765
Sebastian Hollizeck <sebasian.hollizeck@petermac.org>
2021-08-03 16:39:01 +1000

    fixed a bug where a pattern wasnt compiled properly

--- mismatchfinder/core/BamScanner.py
+++ mismatchfinder/core/BamScanner.py
@@ -77,9 +77,11 @@ class BamScanner(Process):
         # this pattern will match the MDString of reads with at least 1, up to a maximum of
         # maxMisMatchesPerRead mismatches in a read (for both SBS and DBS)
         self.MDstrPattern = re.compile(
-            r"\d+([ACGT](0[ACGT])?\d+){",
-            str(minMisMatchesPerRead),
-            "," + str(maxMisMatchesPerRead) + "}",
+            r"\d+([ACGT](0[ACGT])?\d+){"
+            + str(minMisMatchesPerRead)
+            + ","
+            + str(maxMisMatchesPerRead)
+            + "}",
         )
 
     # this function gets all sites of mismatches from any mapped read in a bam
